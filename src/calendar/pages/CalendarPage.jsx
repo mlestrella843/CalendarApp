5ac1000b1912b47ed685e1edc2abca85a5f4e805
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-09-21 11:10:27 -0600

    Adding fetures

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -20,6 +20,20 @@ const events = [{
  }]
 
 const CalendarPage = () => {
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+      console.log( {event, start, end, isSelected });
+            const style = {
+              backgroundColor: '#347CF7',
+              borderRadius: '0px',
+              opacity: '0.8',
+              color: 'white'
+            }        
+            return {
+              style
+            }  
+      }
+  
   return (
     <>
       <Navbar />
@@ -30,6 +44,7 @@ const CalendarPage = () => {
         startAccessor="start"
         endAccessor="end"
         style={{ height: 'calc( 100vh - 80px )' }}
+        eventPropGetter={ eventStyleGetter }
       />
 
     </>
